e31a3aceda695b454dbcc07a7ad483848e2854e9
BigMel <139045418+BigMel1388@users.noreply.github.com>
2026-03-19 14:55:30 +0700

    terrain

--- new-game-project/Scripts/playerMovement.cs
+++ new-game-project/Scripts/playerMovement.cs
@@ -3,4 +3,13 @@ using System;
 
 public partial class NewScript : Node
 {
+    public override void _Ready()
+    {
+
+    }
+
+    public override void _Process(double delta)
+    {
+        g
+    }
 }
